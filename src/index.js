f1b337d66624fba4273c7638043cba20f1a65d8f
RobotCruzr <kimanh.220102@gmail.com>
2024-08-14 16:27:47 +0700

    nodejs

---
+++ src/index.js
@@ -0,0 +1,22 @@
+
+const path = require('path');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const port = 3000
+const handlebars = require('express-handlebars')
+
+app.use(morgan('combined'))
+
+app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
