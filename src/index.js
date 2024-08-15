33c22617cd776ee69c31ea6746b5d242b7a698c5
RobotCruzr <kimanh.220102@gmail.com>
2024-08-15 12:01:15 +0700

    update 15/8/2024

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ const app = express()
 const morgan = require('morgan')
 const port = 3000
 const handlebars = require('express-handlebars')
+const routes = require('./routes')
+
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
 
 app.use(morgan('combined'))
 
+//Temple engine
 app.engine('.hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+//Route
+routes(app)
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
